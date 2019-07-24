0174e26693b37b3888e642d8a15a0a9d0f620eb9
wilson <wilson@laboremus.no>
2019-07-24 17:15:47 +0300

    Removed authentication on POST /api/user endpoint

--- src/public/docs/swagger.options.js
+++ src/public/docs/swagger.options.js
@@ -2,75 +2,75 @@ require('dotenv').config();
 
 let host = process.env.BASE_URL
 if (process.env.PORT) {
-  host += `:${process.env.PORT}`
+    host += `:${process.env.PORT}`
 }
 
 host += '/'
 
 export const swaggerDefinition = {
-  swaggerDefinition: {
-    info: {
-      title: 'PSM Messenger API',
-      version: '0.1.0',
-      description: 'Endpoints to use for PSM Messenger API.',
-      contact: {
-        name: 'Wilson Kiggundu',
-        url: 'http://wilsonie.wordpress.com',
-        email: 'wil.kiggundu@gmail.com',
-      },
-      license: {
-        name: 'Apache 2.0',
-        url: 'https://www.apache.org/licenses/LICENSE-2.0.html',
-      },
+    swaggerDefinition: {
+        info: {
+            title: 'PSM Messenger API',
+            version: '0.1.0',
+            description: 'Endpoints to use for PSM Messenger API.',
+            contact: {
+                name: 'Wilson Kiggundu',
+                url: 'http://wilsonie.wordpress.com',
+                email: 'wil.kiggundu@gmail.com',
+            },
+            license: {
+                name: 'Apache 2.0',
+                url: 'https://www.apache.org/licenses/LICENSE-2.0.html',
+            },
+        },
+        host: host,
+        basePath: '/',
+        produces: ['application/json'],
+        schemes: ['http', 'https'],
+        securityDefinitions: {
+            // basic_auth: {
+            //   type: 'http',
+            //   scheme: 'basic'
+            // },
+
+            JWT: {
+                type: 'http',
+                scheme: 'bearer',
+                bearerFormat: 'JWT'
+            },
+
+            // api_key: {
+            //   type: 'apiKey',
+            //   in: 'header',
+            //   name: 'X-API-Key'
+            // }
+
+        },
+
+        tags: [{
+            name: 'Users',
+            description: 'Handles everything about user signup, authentication and authorization'
+        }, {
+            name: 'Schools',
+            description: 'Handles everything about a school'
+        }, {
+            name: 'Subscriptions',
+            description: 'Handles everything about subscriptions for both school and parents for premium features'
+        }, {
+            name: 'Students',
+            description: 'Handles everything about a student'
+        }, {
+            name: 'Academic Results',
+            description: 'Handles all student results. The results are stored per school, per student, per term, per year'
+        }, {
+            name: 'Notifications',
+            description: 'Handles everything about notifications'
+        }, {
+            name: 'Articles',
+            description: 'Allows users to publish posts, blog articles'
+        }]
+
     },
-    host: host,
-    basePath: '/',
-    produces: ['application/json'],
-    schemes: ['http', 'https'],
-    securityDefinitions: {
-        // basic_auth: {
-        //   type: 'http',
-        //   scheme: 'basic'
-        // },
-
-        JWT: {
-        type: 'http',
-            scheme: 'bearer',
-            bearerFormat: 'JWT'
-      },
-
-        // api_key: {
-        //   type: 'apiKey',
-        //   in: 'header',
-        //   name: 'X-API-Key'
-        // }
-
-    },
-
-    tags: [{
-      name: 'Users',
-      description: 'Handles everything about user signup, authentication and authorization'
-    }, {
-      name: 'Schools',
-      description: 'Handles everything about a school'
-    }, {
-      name: 'Subscriptions',
-      description: 'Handles everything about subscriptions for both school and parents for premium features'
-    }, {
-      name: 'Students',
-      description: 'Handles everything about a student'
-    }, {
-      name: 'Academic Results',
-      description: 'Handles all student results. The results are stored per school, per student, per term, per year'
-    }, {
-      name: 'Notifications',
-      description: 'Handles everything about notifications'
-    }, {
-      name: 'Articles',
-      description: 'Allows users to publish posts, blog articles'
-    }]
-
-  },
-  basedir: __dirname, // app absolute path
-  apis: ['./src/public/docs/spec/*.yml'],
+    basedir: __dirname, // app absolute path
+    apis: ['./src/public/docs/spec/*.yml'],
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,7 +6,7 @@ import passport from "passport";
 export const userRoutes = (app) => {
     app.post('/api/user/login', login);
     app.post('/api/user/reset/password', resetPassword);
-    app.post('/api/user', passport.authenticate('jwt', {session: false}), create);
+    app.post('/api/user', create);
     app.get('/api/user', passport.authenticate('jwt', {session: false}), search);
     app.put('/api/user', passport.authenticate('jwt', {session: false}), update);
     app.delete('/api/user', passport.authenticate('jwt', {session: false}), remove);
